c5dfa77d293022bdca2f12b557ba8f725390da46
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-10-04 15:59:34 +0700

    banner store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
+import getBanner from "@/actions/get-banner";
+import Banner from "@/components/banner";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const banner = await getBanner("7810a535-ed50-4991-a95b-547810f07fe3");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-20">
+        <Banner data={banner} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
