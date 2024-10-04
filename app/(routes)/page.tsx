4148d42bbcb22972b90edcbde7d73aa7d4e05936
Bayu Sugiantoro <bayusugiantoro5@gmail.com>
2024-10-04 22:18:14 +0700

    list product

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import getBanner from "@/actions/get-banner";
+import getProducts from "@/actions/get-products";
 import Banner from "@/components/banner";
+import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
 const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
   const banner = await getBanner("7810a535-ed50-4991-a95b-547810f07fe3");
 
   return (
     <Container>
       <div className="space-y-10 pb-20">
         <Banner data={banner} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Produk unggulan" items={products} />
+        </div>
       </div>
     </Container>
   );
